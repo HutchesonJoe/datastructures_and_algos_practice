568bd0f97c055da18d08f3a1c5b7cd2b6961a041
Joe <hutcheson2001@yahoo.com>
2024-04-11 08:30:29 -0400

    adding first program

---
+++ CodeEx1.py
@@ -0,0 +1,11 @@
+def sorted_squared(array):
+  if array==[]:
+    return []
+  else:
+    for i in array:
+       print(i^2)
+    
+
+sorted_squared([0, 1, 3])
+sorted_squared([-4, 0, 1, 3])
+sorted_squared([])
